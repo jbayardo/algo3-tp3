5a3c46d43940e7d3c595f0bf42642c1ae62caba9
Izikiel <ezequiel.gambaccini@gmail.com>
2015-11-06 21:48:59 -0300

    Arreglo problema de generacion y output. Ahora da dentro los rangos necesarios del problema.

--- random_input_generator.py
+++ random_input_generator.py
@@ -5,11 +5,11 @@ from itertools import combinations
 
 
 def random_input():
-    n = randint(1, 100)
+    n = randint(10, 100)
     m = randint(1, ((n-1)*n)/2)
     c = n
 
-    colors = {k: set([randint(1, c) for _ in xrange(randint(1, c))]) for k in xrange(n)}
+    colors = {k: set([randint(1, c-1) for _ in xrange(randint(1, c-1))]) for k in xrange(n)}
 
     all_edges = list(combinations(xrange(n), 2))
 
@@ -20,12 +20,12 @@ def random_input():
     header = "%d %d %d\n" % (n, m, c)
     color_out = []
 
-    for c, vals in colors.iteritems():
+    for _, vals in colors.iteritems():
         s_len = "%d " % len(vals)
         s_vals = " ".join(["%d" % x for x in vals])
-        color_out.append(s_len + s_vals)
+        color_out.append(s_len + s_vals + "\n")
 
-    print header + "\n".join(color_out) + edges
+    print header + "".join(color_out) + edges
 
 if __name__ == '__main__':
     random_input()
