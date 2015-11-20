726e65fc66942c2c507e4192bb72ee02b42345ca
Izikiel <ezequiel.gambaccini@gmail.com>
2015-11-20 17:14:31 -0300

    Agrego archivo para filtrar headers de csv mal parseados

---
+++ filter_header.py
@@ -0,0 +1,24 @@
+#!/usr/bin/pypy
+import sys
+
+def copy_filtered_headers(filename):
+    with open(filename) as source:
+        with open("copy_"+filename, "w") as destiny:
+            header = source.readline()
+            destiny.write(header)
+            while True:
+                line_s = source.readline()
+                if len(line_s) == 0:
+                    break
+                elif line_s == header:
+                    continue
+                else:
+                    destiny.write(line_s)
+
+    print "copy_"+filename
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print "filter_header.py archivo.csv"
+    copy_filtered_headers(sys.argv[1])
